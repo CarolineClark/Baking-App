9bc25bcf9a0b29107937b1e3b775edabef0240c0
CarolineClark <carolinejaneclark@gmail.com>
2017-09-28 13:00:38 +0100

    Added Idling Resources

--- app/src/androidTest/java/io/liney/bakingapp/MainActivityTest.java
+++ app/src/androidTest/java/io/liney/bakingapp/MainActivityTest.java
@@ -1,12 +1,15 @@
 package io.liney.bakingapp;
 
 import android.content.Intent;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingResource;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
-import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,7 +19,6 @@ import io.liney.bakingapp.matchers.RecyclerViewMatcher;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -28,27 +30,44 @@ import static org.hamcrest.Matchers.allOf;
 public class MainActivityTest {
 
     @Rule
-    public IntentsTestRule<MainActivity> mMainActivityTestRule = new IntentsTestRule<>(MainActivity.class);
+    public IntentsTestRule<MainActivity> mActivityRule = new IntentsTestRule<>(MainActivity.class, false, false);
+    private IdlingResource mIdlingResource;
 
     public static RecyclerViewMatcher withRecyclerView(final int recyclerViewId) {
         return new RecyclerViewMatcher(recyclerViewId);
     }
 
+    @Before
+    public void registerIdlingResource() {
+        Intent startIntent = new Intent();
+        startIntent.putExtra("test", true);
+        mActivityRule.launchActivity(startIntent);
+        mIdlingResource = mActivityRule.getActivity().getIdlingResource();
+        Espresso.registerIdlingResources(mIdlingResource);
+    }
+
+    @After
+    public void unregisterIdlingResource() {
+        if (mIdlingResource != null) {
+            Espresso.unregisterIdlingResources(mIdlingResource);
+        }
+    }
+
     @Test
-    public void clickRecipeGoesToRecipeDetailActivityScreen() {
+    public void clickRecipeGoesToRecipeDetailActivityScreen() throws InterruptedException {
         // when
         onView(withId(R.id.main_recipe_recycler_view))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 
         // then
         intended(hasComponent(RecipeDetailActivity.class.getName()));
     }
 
     @Test
-    public void clickRecipeShowsRecipeNameAtTopOfNextScreen() {
+    public void clickRecipeShowsRecipeNameAtTopOfNextScreen() throws InterruptedException {
         // when
-        ViewInteraction recyclerView = onView(allOf(withId(R.id.main_recipe_recycler_view), isDisplayed()));
-        recyclerView.perform(actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.main_recipe_recycler_view))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 
         // then
         ViewInteraction textView = onView(allOf(withId(R.id.recipe_name_text_view), isDisplayed()));
--- app/src/main/java/io/liney/bakingapp/MainActivity.java
+++ app/src/main/java/io/liney/bakingapp/MainActivity.java
@@ -2,10 +2,14 @@ package io.liney.bakingapp;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
+import android.support.test.espresso.IdlingResource;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.Toast;
 import java.util.List;
 import butterknife.BindView;
@@ -21,16 +25,27 @@ public class MainActivity extends AppCompatActivity implements RecipeAdapter.Rec
     @BindView(R.id.main_recipe_recycler_view) RecyclerView mRecipeRecyclerView;
     private RecipeAdapter mAdapter;
 
+    @Nullable
+    private SimpleIdlingResource mIdlingResource;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        if (getIntent().getBooleanExtra("test", false)) {
+            // in a test, instantiate the idling resource
+            mIdlingResource = new SimpleIdlingResource();
+        }
+
         mAdapter = new RecipeAdapter(this, this);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         mRecipeRecyclerView.setLayoutManager(layoutManager);
         mRecipeRecyclerView.setAdapter(mAdapter);
+
+        Log.d(TAG, "Idling resource called! " + mIdlingResource);
+        setIdleState(false);
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://d17h27t6h515a5.cloudfront.net/")
@@ -48,13 +63,21 @@ public class MainActivity extends AppCompatActivity implements RecipeAdapter.Rec
                     return;
                 }
                 mAdapter.setRecipes(recipes);
+                setIdleState(true);
             }
 
             @Override
             public void onFailure(@NonNull Call<List<RecipePojo>> call, @NonNull Throwable t) {
                 Toast.makeText(MainActivity.this, "Failed to fetch recipes. Do you have internet connection?", Toast.LENGTH_LONG).show();
+                setIdleState(true);
             }
         });
+    }
+
+    private void setIdleState(boolean isIdleNow) {
+        if (mIdlingResource != null) {
+            mIdlingResource.setIdleState(isIdleNow);
+        }
     }
 
     @Override
@@ -62,5 +85,14 @@ public class MainActivity extends AppCompatActivity implements RecipeAdapter.Rec
         Intent intent = new Intent(this, RecipeDetailActivity.class);
         intent.putExtra("recipe", recipeData);
         startActivity(intent);
+    }
+
+    @VisibleForTesting
+    @NonNull
+    public IdlingResource getIdlingResource() {
+        if (mIdlingResource == null) {
+            mIdlingResource = new SimpleIdlingResource();
+        }
+        return mIdlingResource;
     }
 }
